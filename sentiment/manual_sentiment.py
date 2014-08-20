a58e17bd6cf8e5a2e452d899e8cb6e17c04c99af
pdbpdb <pablobesada@gmail.com>
2014-08-20 01:33:52 -0300

    cambios

--- sentiment/manual_sentiment.py
+++ sentiment/manual_sentiment.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
 from bson import ObjectId
-mongoclient = MongoClient()
+mongoclient = MongoClient("prodedelmundial.com.ar")
 db = mongoclient['test']
-
+datasift = mongoclient['datasiftmongodb']
 from flask import Flask, render_template, request
+import flask 
 
 app = Flask(__name__)
 
@@ -16,10 +17,26 @@ def home():
 @app.route("/rate", methods=['POST'])
 def rate():
     print request.form['id'], request.form['sent']
-    print db.tweets.update({"_id": ObjectId(request.form['id'])}, {"$set": {"x_sentiment": request.form['sent']}})
-    
+    print db.tweets.update({"_id": ObjectId(request.form['id'])}, {"$set": {"x_sentiment": request.form['sent']}})    
     return "OK"
-    
+
+@app.route("/explore", methods=['GET'])
+def explore():
+    cnt = 30
+    page = int(request.args.get("page", "1"))
+    if page < 1: page = 1
+    search = request.args.get("search", "")
+    cond = {}
+    if search: cond = {"$text": {"$search": search}}
+    tweets = datasift.my_first_test.find(cond).skip((page-1)*cnt).limit(30)
+    return render_template('explore_tweets.html', tweets=tweets, page=page)
+
+@app.route("/showtweet", methods=['GET'])
+def showtweet():
+    tweet = datasift.my_first_test.find_one({"_id": ObjectId(request.args['id'])})
+    tweet['_id'] = str(tweet['_id'])
+    return flask.jsonify(**tweet)
+  
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0")
