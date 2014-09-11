90416a66b9e08cffeced99d4dc5952bc3a5fd8a3
pdbpdb <pablobesada@gmail.com>
2014-09-11 02:16:09 -0300

    cambios

---
+++ script.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from brandclassifier import BrandClassifier
+from datetime import datetime
+import re
+local = MongoClient()
+remoto = MongoClient("prodedelmundial.com.ar")
+monitor_local = local['monitor']
+monitor_remoto = remoto['monitor']
+monitor_remoto.authenticate("monitor", "monitor678")
+
+doc = monitor_local.accounts.find_one({"name": "SiVale"})
+print monitor_remoto.accounts.save(doc)
