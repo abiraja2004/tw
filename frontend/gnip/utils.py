50ce8a77eeb2fcc99cd1d6558e91f831e1847e28
pdbpdb <pablobesada@gmail.com>
2015-03-07 15:20:16 -0300

    cambios

--- frontend/gnip/utils.py
+++ frontend/gnip/utils.py
@@ -1,14 +1,14 @@
 import threading
 
 class MyThread(threading.Thread):
-    instances = []
+    #instances = []
     def __init__(self):
         threading.Thread.__init__(self)
-        MyThread.instances.append(self)
+        #MyThread.instances.append(self)
         self.setName(self.__class__.__name__)
         
     @classmethod
     def checkFinalization(clsobj):
-        for t in MyThread.instances:
-            if t.isAlive():
+        for t in threading.enumerate():
+            if isinstance(t, MyThread):
                 print "Thread Alive: %s " % t.getName()
